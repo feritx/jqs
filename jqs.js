6c927f83c66c1f1014e3e52dd3ac8ee309e0b0b6
Ferit <feritx@gmail.com>
2019-11-21 06:13:40 +0300

    yeni commit 2

--- jqs.js
+++ jqs.js
@@ -2,7 +2,11 @@ var jqs=function(css){
 	
 	if(!css)
 		return false;
+	if(typeof css object){
+		
+	}
   return document.querySelectorAll(css);
+  
   //it will work like jquery without storage
   
 }
